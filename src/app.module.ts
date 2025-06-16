5fd8ff2dde4775a8f3d1629bbdf69a27dc77cb38
sijalneupane <sijalneupane5@gmail.com>
2025-06-16 18:31:36 +0545

    feat: TypeOrm setup in app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,23 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CarsModule } from './cars/cars.module';
 import { CoursesModule } from './courses/courses.module';
+import { Car } from './cars/entities/car.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [CarsModule, CoursesModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [TypeOrmModule.forRoot(
+    {
+  "type": "postgres",
+  "host": "localhost",
+  "port": 5432,
+  "username": "postgres",
+  "password": "root123",
+  "database": "nestjs_tutorial",
+  "entities": [Car],
+  "synchronize": true
+}
+  ), CarsModule, CoursesModule],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
