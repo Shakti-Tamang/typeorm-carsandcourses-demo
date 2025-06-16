18132af559635648b68fe8a39bd73e949e8b52e5
sijalneupane <sijalneupane5@gmail.com>
2025-06-16 18:26:05 +0545

    Feat: Courses module created

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CarsModule } from './cars/cars.module';
+import { CoursesModule } from './courses/courses.module';
 
 @Module({
-  imports: [CarsModule],
+  imports: [CarsModule, CoursesModule],
   controllers: [AppController],
   providers: [AppService],
 })
