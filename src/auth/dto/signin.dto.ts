1e79e8013152e5dd05e7efa2a693686e860afcde
sijalneupane <sijalneupane5@gmail.com>
2025-06-17 16:15:53 +0545

    Feat: class-validator installed and used in signin.dto.ts

--- src/auth/dto/signin.dto.ts
+++ src/auth/dto/signin.dto.ts
@@ -1,4 +1,9 @@
-export interface SignInDto{
+import { IsNotEmpty } from "class-validator";
+
+export class SignInDto{
+    @IsNotEmpty()
     username: string;
+
+    @IsNotEmpty()
     password: string;
 }
