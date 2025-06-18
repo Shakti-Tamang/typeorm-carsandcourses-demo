0bfdd77b44192317098d6395efe17fe2298e89ec
sijalneupane <sijalneupane5@gmail.com>
2025-06-18 15:22:47 +0545

    Fix: Implemeted user create function using TypeORM(Repositroy) instead of memory data

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -6,9 +6,9 @@ import { SignInDto } from './dto/signin.dto';
 export class AuthController {
   constructor(private authService: AuthService) {}
 
-  @HttpCode(HttpStatus.OK)
-  @Post('login')
-  signIn(@Body() signInDto: SignInDto) {
-    return this.authService.signIn(signInDto.username, signInDto.password);
-  }
+  // @HttpCode(HttpStatus.OK)
+  // @Post('login')
+  // signIn(@Body() signInDto: SignInDto) {
+  //   return this.authService.signIn(signInDto.username, signInDto.password);
+  // }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,23 +1,42 @@
 import { Injectable } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-users.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './entities/users.entity';
+import { Repository } from 'typeorm';
+import { create } from 'domain';
 
-export type User = any;
+// export type User = any;
 
 @Injectable()
 export class UsersService {
-  private readonly users = [
-    {
-      userId: 1,
-      username: 'john',
-      password: 'changeme',
-    },
-    {
-      userId: 2,
-      username: 'maria',
-      password: 'guess',
-    },
-  ];
+  constructor(
+    @InjectRepository(User)
+    private userRepository: Repository<User>){}
+  // private readonly users = [
+  //   {
+  //     id: 1,
+  //     userId: 1,
+  //     username: 'john',
+  //     password: 'changeme',
+  //     name: 'John Doe',
+  //     email: 'john@example.com',
+  //   },
+  //   {
+  //     id: 2,
+  //     userId: 2,
+  //     username: 'maria',
+  //     password: 'guess',
+  //     name: 'Maria Smith',
+  //     email: 'maria@example.com',
+  //   },
+  // ];
 
-  async findOne(username: string): Promise<User | undefined> {
-    return this.users.find(user => user.username === username);
+  // async findOne(username: string): Promise<User | undefined> {
+  //   return this.users.find(user => user.username === username);
+  // }
+   createUser(createUserDto:CreateUserDto):Promise<User>{
+const user=this.userRepository.create(createUserDto);
+return this.userRepository.save(user);
+    
   }
 }
